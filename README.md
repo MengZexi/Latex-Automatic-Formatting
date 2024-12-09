16908f9266afc8efc2bdc5bdef03096ba704e6d4
MengZexi <mengzexi@stu.ouc.edu.cn>
2024-12-09 17:50:24 +0800

    add_license

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Latex-Automatic-Formatting
+
+
 Typesetting the contents of the clipboard
 
-This is a empty project!!!
---
+++ latex_automatic_format.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Latex_Automatic Formatting
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Typesetting the contents of the clipboard
+// @author       Mozikiy
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @license      GNU GPLv3
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("hello world");
+})();
